5536d8ff1649fa299c5cfb6023d0ba8734b07a32
topgatechstudent <133879937+topgatechstudent@users.noreply.github.com>
2023-07-20 17:31:26 +0100

    greatly improved the game with graphics and animation
    I greatly improved the look of the game by styling the site using CSS. I also wrote animations for each button and implemented them using a function in JS.

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
+let rockButton = document.getElementById("rock");
+let paperButton = document.getElementById("paper");
+let scissorButton = document.getElementById("scissors");
 function gameHandler() {
-    let rockButton = document.getElementById("rock");
-    let paperButton = document.getElementById("paper");
-    let scissorButton = document.getElementById("scissors");
     const computerOptions = ["rock", "paper", "scissors"];
     const playerOptions = [rockButton, paperButton, scissorButton];
 
     playerOptions.forEach(option => option.addEventListener("click", function () {
         const computerNumber = Math.floor(Math.random() * 3);
         let computerChoice = computerOptions[computerNumber];
-        winnerCalculator(option.innerText, computerChoice);
+        winnerCalculator(option.value, computerChoice);
     }));
+}
 
+function animationHandler(winner, loser) {
+    winner.classList.add("bounce");
+    loser.classList.add("bounce");
+    setTimeout(function () {
+        winner.classList.remove("bounce");
+        loser.classList.remove("bounce");
+        loser.classList.add("lose");
+        setTimeout(function () {
+            loser.classList.remove("lose");
+        }, 1000);
+    }, 3000);
 }
 
 function winnerCalculator(playerChoice, computerChoice) {
@@ -19,20 +31,26 @@ function winnerCalculator(playerChoice, computerChoice) {
     } else if (playerChoice == "rock") {
         if (computerChoice == "paper") {
             alert("You Lose");
+            animationHandler(paperButton, rockButton);
         } else {
             alert("You Win");
+            animationHandler(rockButton, scissorButton);
         }
     } else if (playerChoice == "paper") {
         if (computerChoice == "scissors") {
             alert("You Lose");
+            animationHandler(scissorButton, paperButton);
         } else {
             alert("You Win");
+            animationHandler(paperButton, rockButton);
         }
     } else {
         if (computerChoice == "rock") {
             alert("You Lose");
+            animationHandler(rockButton, scissorButton);
         } else {
             alert("You Win");
+            animationHandler(scissorButton, paperButton);
         }
     }
 }
